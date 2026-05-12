0639698d33809cab193b2f4bc6f8089d25e7616e
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-05-13 00:49:14 +0530

    Fixing Expire ttl mistake

--- Day26/middleware/Ratelimiter.js
+++ Day26/middleware/Ratelimiter.js
@@ -3,7 +3,7 @@ const crypto = require("crypto");
 
 const Ratelimiter = async (req, res, next) => {
   try {
-    const key = `${req.ip}`;
+    const key = req.ip;
     const Window = Date.now() - 3600000;
     const uniquevalue = crypto.randomUUID();
 
@@ -15,12 +15,12 @@ const Ratelimiter = async (req, res, next) => {
     await redisClient.zRemRangeByScore(key, 0, Window);
 
     const count = await redisClient.zCard(key);
-
-    if (count > 10) {
+    console.log(count);
+    if (count > 30) {
       throw new Error("Too many request");
     }
 
-    await redisClient.expire(key, Window);
+    await redisClient.expire(key, 3600);
 
     next();
   } catch (err) {
